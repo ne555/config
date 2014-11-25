af4a21d31d6491f022deadd947db8a620ea2db21
aldo_bedrij <wbedrij@gmail.com>
2014-11-25 15:48:58 -0300

    stashed changes
    bashrc
    	new aliases
    
    git/ignore
    	don't ignore sql scripts
    
    inputrc
    	colored tabcompletion
    
    vimrc
    	compile command
    
    mpd
    	midi support (test in parabola)
    
    stumpwm
    	battery
    	model line
    	don't steal focus (in test)
    
    xinitrc
    	ratpoison is default

--- emacs.el
+++ emacs.el
@@ -1,3 +1,4 @@
+
 (custom-set-variables
 	;; custom-set-variables was added by Custom.
 	;; If you edit it by hand, you could mess it up, so be careful.
@@ -8,6 +9,7 @@
 	'(revert-without-query 
 		(quote ("t"))
 	)
+	'(tab-width 4)
 )
 (custom-set-faces
 	;; custom-set-faces was added by Custom.
@@ -16,3 +18,21 @@
 	;; If there is more than one, they won't work right.
 )
 (setq linum-format "%4d\u2502 ")
+(setq tab-stop-list (number-sequence 4 120 4))
+
+;; (require 'compile)
+;; (add-hook 'c++-mode-hook
+;; 	(lambda ()
+;; 		(set (make-local-variable 'compile-command )
+;; 			(format "%s %s"
+;; 				"g++ -W{all,extra,pedantic} -std=c++11"
+;; 				(file-name-nondirectory buffer-file-name)
+;; 			)
+;; 		 )
+;; 	)
+;; )
+
+(setq compilation-read-command nil)
+
+(add-to-list 'load-path "~/.local/share/emacs")
+(require 'smart-compile)
