df4862a489711c46418408f14f7d33f868a2aaa3
aldo_bedrij <wbedrij@gmail.com>
2016-06-30 16:15:51 -0300

    alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,35 @@
+alias g++='g++ -fno-diagnostics-show-caret'
+alias gcc='gcc -fno-diagnostics-show-caret'
+alias grep='grep --color=auto'
+alias ls='ls --color=auto'
+alias nload='nload -u K -U K'
+alias octave='rlwrap octave-cli -q'
+alias pacman='pacman --color always'
+alias ping='ping -4'
+alias rename='perl-rename'
 
+alias openfoam='source /opt/OpenFOAM/OpenFOAM-2.4.0/etc/bashrc'
+
+alias r='R --no-save --quiet'
+
+alias startx='startx ~/.config/xinitrc'
+alias stellarium='stellarium --config-file ~/.config/stellarium.ini --user-dir ~/.local/share/stellarium'
+alias w3m='w3m -config ~/.config/w3m/config'
+
+#weird bugs
+#(clang_complete)
+alias vim="vim 2>/dev/null"
+
+# Using .config as config directory
+alias amule='amule --config-dir=~/.config/amule'
+alias dgen='dgen -r ~/.config/dgenrc'
+alias irssi='irssi --config ~/.config/irssi --home=~/.local/share/irssi'
+alias lingot='lingot -c ~/.config/lingot'
+alias mutt='mutt -F ~/.config/muttrc'
+alias ncmpcpp='ncmpcpp --config ~/.config/ncmpcpp --bindings ~/.config/ncmpcpp.bindings'
+alias pcsx2='pcsx2 --cfg=~/.config/pcsx2'
+alias pcsxr='pcsxr -cfg=~/.config/pcsxr'
+alias snes9x='snes9x -conf ~/.config/snes9x'
+alias startx='startx ~/.config/xinitrc'
+alias tmux='tmux -u -2 -f ~/.config/tmux.conf'
+#alias vim='vim -u ~/.config/vimrc'
