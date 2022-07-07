0ed7a1587213f1b67150df0e65ecf18cce38b3a5
Walter Bedrij <mpa.santafe.gov.ar>
2022-07-07 08:43:29 -0300

    remove role="dialog" elements

--- qutebrowser/scripts/js/no_popup.js
+++ qutebrowser/scripts/js/no_popup.js
@@ -24,4 +24,7 @@ function remove_tag(tag_name){
 remove_class('RnEpo _Yhr4 ')
 remove_class('ml-webforms-popup-overlay')
 remove_tag('iframe')
+remove_element(() => {
+    return document.querySelectorAll('div[role="dialog"]')
+})
 document.body.style.overflow="auto";
