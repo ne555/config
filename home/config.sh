d85859023d761052916e3fca52017cdd951942ad
Aldo Jirdeb <mpa>
2022-12-07 07:23:08 -0300

    statnot as notifier, nvim as editor

--- home/config.sh
+++ home/config.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-#create symbolic links to the configuration files that can't be used with XDG 
+# create symbolic links to the configuration files that can't be used with XDG
+# execute from $HOME
 
 program="$0"
 directory="$(dirname ${program})"
 ignore="$(basename ${program})"
 for file in $(ls "${directory}" -I "${ignore}"); do
-	#it will overwrite anything that you've got 
+	#it will overwrite anything that you've got
 	ln -sf "${directory}/${file}" ~/."${file}"
-done 
+done
