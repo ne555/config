a80bfae236bdae9db60d99e13d87eefd7de2c9d1
Walter Bedrij <mpa.santafe.gov.ar>
2023-02-03 10:51:07 -0300

    ironrepl config

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -7,15 +7,14 @@ iron.setup {
     -- Your repl definitions come here
     repl_definition = {
       sh = {
-        command = {"bash"}
-      },
-      python = {
-        command = {"python"}
+        -- Can be a table or a function that
+        -- returns a table (see below)
+        command = {"zsh"}
       }
     },
     -- How the repl window will be displayed
     -- See below for more information
-    repl_open_cmd = require('iron.view').right(0.5),
+    repl_open_cmd = require('iron.view').split.vertical.botright(0.5),
   },
   -- Iron doesn't set keymaps by default anymore.
   -- You can set them here or manually add keymaps to the functions in iron.core
@@ -35,7 +34,18 @@ iron.setup {
   },
   -- If the highlight is on, you can change how it looks
   -- For the available options, check nvim_set_hl
-  -- highlight = {
-  --   italic = true
-  -- }
+  highlight = {
+    italic = true
+  },
+  ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
 }
+
+-- iron also has a list of commands, see :h iron-commands for all available commands
+vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
+vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
+vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+
+require('packer').startup(function(use)
+    use 'neoclide/coc.nvim'
+end)
