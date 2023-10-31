9a88c79796290e03ff73482b0ba68d7101a75cbe
Walter Bedrij <wbedrij@mpa.santafe.gov.ar>
2023-10-31 11:01:20 -0300

    direct shortcuts with xbindkeys

---
+++ xbindkeysrc.scm
@@ -0,0 +1,8 @@
+(xbindkey
+  '(control F12)
+  "scrot --select --freeze - | xclip -selection clipboard -t image/png")
+
+
+(xbindkey
+  '(F12)
+  "scrot - | xclip -selection clipboard -t image/png")
