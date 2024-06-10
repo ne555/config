6e965502cba8524ea9d61faac3f5da12e9821c83
Walter Bedrij <wbedrij@mpa.santafe.gov.ar>
2024-06-10 10:02:16 -0300

    wipeout as user command

--- nvim/lua/functions/wipeout.lua
+++ nvim/lua/functions/wipeout.lua
@@ -6,20 +6,26 @@ local function print_table(table_)
     end
 end
 
-function wipeout()
-    local buffers = vim.api.nvim_list_bufs()
-    local reverse = {}
-    for K, x in pairs(buffers) do
-        reverse[x] = K
-    end
 
-    -- remove active buffers from list
-    for _, x in pairs(vim.api.nvim_list_wins()) do
-        local b = vim.api.nvim_win_get_buf(x)
-        buffers[reverse[b]] = nil
-    end
+vim.api.nvim_create_user_command(
+    'Wipeout',
+    --'remove buffers not in any tab/window'
+    function()
+        local buffers = vim.api.nvim_list_bufs()
+        local reverse = {}
+        for K, x in pairs(buffers) do
+            reverse[x] = K
+        end
 
-    for _, x in pairs(buffers) do
-        vim.api.nvim_buf_delete(x, {})
-    end
-end
+        -- remove active buffers from list
+        for _, x in pairs(vim.api.nvim_list_wins()) do
+            local b = vim.api.nvim_win_get_buf(x)
+            buffers[reverse[b]] = nil
+        end
+
+        for _, x in pairs(buffers) do
+            vim.api.nvim_buf_delete(x, {})
+        end
+    end,
+    {}
+)
